0599ba77a1e323f712b1c26af87a7b5baf7312d6
Michael O'Rafferty <morafferty9@gmail.com>
2020-10-19 21:25:14 +0100

    Add files via upload
    Running this script usually involves downloading python, installing libraries, then running it in the command prompt or some other IDE. Not exactly easy for anyone who is not used to programming. This batch file will both install libraries and run the programme with only a double click. And a shortcut can also be set up to allow for convenient access from desktop

---
+++ scores.bat
@@ -0,0 +1,7 @@
+@echo off
+pip -q --disable-pip-version-check install win10toast
+pip -q --disable-pip-version-check install requests
+pip -q --disable-pip-version-check install bs4
+pip -q --disable-pip-version-check install datetime
+cd C:/Users/moraf/Documents/Scripts
+python  Scores.py
