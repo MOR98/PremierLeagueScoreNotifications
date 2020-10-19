61a9286bb7907359468c191c0707ca0a8e870395
Michael O'Rafferty <morafferty9@gmail.com>
2020-10-19 21:50:34 +0100

    Update scores.bat

--- scores.bat
+++ scores.bat
@@ -3,5 +3,6 @@ pip -q --disable-pip-version-check install win10toast
 pip -q --disable-pip-version-check install requests
 pip -q --disable-pip-version-check install bs4
 pip -q --disable-pip-version-check install datetime
+pip -q --disable-pip-version-check install os
 cd C:/Users/moraf/Documents/Scripts
 python  Scores.py
